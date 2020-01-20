0f3d43f1d151a8fe42d9e45e0180c7fb17331563
Tahminur Rahman <trahman1@pride.hofstra.edu>
2020-01-20 16:27:35 -0500

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Tahminur Rahman on 1/20/20.
+//  Copyright © 2020 Tahminur Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
